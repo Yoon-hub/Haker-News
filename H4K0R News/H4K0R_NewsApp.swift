d1c28d4a523f529cd41ea48dec9ca243f7a113e8
최윤제 <kong4170@naver.com>
2022-05-18 16:36:35 +0900

    first commit

---
+++ H4K0R News/H4K0R_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4K0R_NewsApp.swift
+//  H4K0R News
+//
+//  Created by 최윤제 on 2022/05/18.
+//
+
+import SwiftUI
+
+@main
+struct H4K0R_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
